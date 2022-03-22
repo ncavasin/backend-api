3967461892c97fa023bf612a7b30b5f05e2f14f1
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-22 01:04:24 -0300

    add skeleton of tests for UserServiceImplTest

---
+++ src/test/java/com/sip/api/services/UserServiceImplTest.java
@@ -0,0 +1,53 @@
+package com.sip.api.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceImplTest {
+
+    @Test
+    public void shouldCreateUser(){
+
+    }
+
+    @Test
+    public void shouldNotCreateWhenWeakPassword(){
+
+    }
+
+    @Test
+    public void shouldNotCreateWhenDuplicatedUsername(){
+
+    }
+
+    @Test
+    public void shouldNotCreateWhenDuplicatedEmail(){
+
+    }
+
+    @Test
+    public void shouldUpdateUsername(){
+
+    }
+
+    @Test
+    public void shouldUpdateEmail(){
+
+    }
+
+    @Test
+    public void shouldUpdatePassword(){
+
+    }
+
+    @Test
+    public void shouldDeactivateUser(){
+
+    }
+
+    @Test
+    public void shouldDeleteUser(){
+
+    }
+}
