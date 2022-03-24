8e6365767fdadce461a5690bc637bf739588ce98
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 13:07:05 -0300

    user Module: replace UserDto methods with UserConverter in UserController

--- src/main/java/com/sip/api/controllers/UserController.java
+++ src/main/java/com/sip/api/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.sip.api.controllers;
 
+import com.sip.api.domains.user.UserConverter;
+import com.sip.api.dtos.user.UserCreationDto;
 import com.sip.api.dtos.user.UserDto;
 import com.sip.api.dtos.user.UserEmailDto;
 import com.sip.api.dtos.user.UserPasswordDto;
@@ -19,32 +21,32 @@ public class UserController {
 
     @GetMapping("/all")
     public List<UserDto> findAll() {
-        return UserDto.entityToDto(userService.findAll());
+        return UserConverter.entityToDto(userService.findAll());
     }
 
     @GetMapping("/{userId}")
     public UserDto findUser(@PathVariable("userId") String userId) {
-        return UserDto.entityToDto(userService.findById(userId));
+        return UserConverter.entityToDto(userService.findById(userId));
     }
 
     @PostMapping("")
-    public UserDto addUser(@Validated @RequestBody UserDto userCreationDto) {
-        return UserDto.entityToDto(userService.createUser(userCreationDto));
+    public UserDto addUser(@Validated @RequestBody UserCreationDto userCreationDto) {
+        return UserConverter.entityToDto(userService.createUser(userCreationDto));
     }
 
     @PutMapping("/email/{userId}")
     public UserDto updateEmail(@PathVariable("userId") String userId, @Validated @RequestBody UserEmailDto userEmailDto) {
-        return UserDto.entityToDto(userService.updateEmail(userId, userEmailDto));
+        return UserConverter.entityToDto(userService.updateEmail(userId, userEmailDto));
     }
 
     @PutMapping("/password/{userId}")
     public UserDto updatePassword(@PathVariable("userId") String userId, @Validated @RequestBody UserPasswordDto userPasswordDto) {
-        return UserDto.entityToDto(userService.updatePassword(userId, userPasswordDto));
+        return UserConverter.entityToDto(userService.updatePassword(userId, userPasswordDto));
     }
 
     @PutMapping("/activate/{userId}")
     public UserDto activateUser(@PathVariable("userId") String userId) {
-        return UserDto.entityToDto(userService.activateUser(userId));
+        return UserConverter.entityToDto(userService.activateUser(userId));
     }
 
     @DeleteMapping("/deactivate/{userId}")
