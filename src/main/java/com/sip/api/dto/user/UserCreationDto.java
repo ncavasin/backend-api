0d7290f1adc70e225e1b3cfcabef3c5216f38f54
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 10:49:36 -0300

    userModule: adding user creation dto

---
+++ src/main/java/com/sip/api/dto/user/UserCreationDto.java
@@ -0,0 +1,32 @@
+package com.sip.api.dto.user;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreationDto {
+    @Size(max = 8)
+    private int dni;
+
+    @NonNull
+    private String password;
+
+    @Email
+    @NonNull
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    private int phone;
+
+    private String zipCode;
+}
