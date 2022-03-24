e616cff8984a72863f90844576fa1b6049974baf
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 11:27:39 -0300

    user Module: expose basic endpoints

--- src/main/java/com/sip/api/controllers/UserController.java
+++ src/main/java/com/sip/api/controllers/UserController.java
@@ -1,9 +1,13 @@
-package com.sip.api.controller;
+package com.sip.api.controllers;
 
-import com.sip.api.service.UserService;
+import com.sip.api.dtos.user.UserDto;
+import com.sip.api.dtos.user.UserEmailDto;
+import com.sip.api.services.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -13,38 +17,33 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/all")
-    public List<UserDto> findAll(){
-        return UserDto.from(userService.findAll());
+    public List<UserDto> findAll() {
+        return UserDto.entityToDto(userService.findAll());
     }
 
     @GetMapping("/{userId}")
     public UserDto findUser(@PathVariable("userId") String userId) {
-        return UserDto.from(userService.findById(userId));
+        return UserDto.entityToDto(userService.findById(userId));
     }
 
     @PostMapping("")
-    public UserDto addUser(@Validated @RequestBody UserCreationDto userCreationDto) {
-        return UserDto.from(userService.createUser(userCreationDto));
-    }
-
-    @PutMapping("/username/{userId}")
-    public UserDto updateUsername(@PathVariable("userId") String userId, @Validated @RequestBody UserUsernameDto userUsernameDto) {
-        return UserDto.from(userService.updatUsername(userId, userUsernameDto));
+    public UserDto addUser(@Validated @RequestBody UserDto userCreationDto) {
+        return UserDto.entityToDto(userService.createUser(userCreationDto));
     }
 
     @PutMapping("/email/{userId}")
     public UserDto updateEmail(@PathVariable("userId") String userId, @Validated @RequestBody UserEmailDto userEmailDto) {
-        return UserDto.from(userService.updateEmail(userId, userEmailDto));
+        return UserDto.entityToDto(userService.updateEmail(userId, userEmailDto));
     }
 
     @PutMapping("/password/{userId}")
     public UserDto updatePassword(@PathVariable("userId") String userId, @Validated @RequestBody UserPasswordDto userPasswordDto) {
-        return UserDto.from(userService.updatePassword(userId, userPasswordDto));
+        return UserDto.entityToDto(userService.updatePassword(userId, userPasswordDto));
     }
 
     @PutMapping("/activate/{userId}")
     public UserDto activateUser(@PathVariable("userId") String userId) {
-        return UserDto.from(userService.activateUser(userId));
+        return UserDto.entityToDto(userService.activateUser(userId));
     }
 
     @DeleteMapping("/deactivate/{userId}")
