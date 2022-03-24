97b174f1de7c765bb0b381e971da3ac0c63653ed
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 13:07:49 -0300

    user Module: define business logic

--- src/main/java/com/sip/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/sip/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.sip.api.services.impl;
 
+import com.sip.api.domains.enums.UserStatus;
+import com.sip.api.domains.user.UserConverter;
+import com.sip.api.dtos.user.UserCreationDto;
 import com.sip.api.dtos.user.UserPasswordDto;
 import com.sip.api.domains.user.User;
-import com.sip.api.dtos.user.UserDto;
 import com.sip.api.dtos.user.UserEmailDto;
+import com.sip.api.exceptions.BadRequestException;
+import com.sip.api.exceptions.NotFoundException;
+import com.sip.api.repositories.UserRepository;
 import com.sip.api.services.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,43 +20,83 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
     @Override
     public List<User> findAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public User findById(String userId) {
-        return null;
+        return userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
     }
 
     @Override
-    public User createUser(UserDto userCreationDto) {
-        return null;
+    public User createUser(UserCreationDto userCreationDto) {
+        checkUserRules(userCreationDto.getDni(), userCreationDto.getEmail(), userCreationDto.getPassword());
+        // TODO encrypt pwd
+        return userRepository.save(UserConverter.dtoToEntity(userCreationDto));
     }
 
     @Override
     public User updateEmail(String userId, UserEmailDto userEmailDto) {
-        return null;
+        validateEmail(userEmailDto.getEmail());
+        User user = findById(userId);
+        user.setEmail(userEmailDto.getEmail());
+        return userRepository.save(user);
     }
 
     @Override
     public User updatePassword(String userId, UserPasswordDto userPasswordDto) {
-        return null;
+        validatePassword(userPasswordDto.getPassword());
+        // TODO encrypt pwd
+        User user = findById(userId);
+        user.setPassword(userPasswordDto.getPassword());
+        return userRepository.save(user);
     }
 
     @Override
     public User activateUser(String userId) {
-        return null;
+        User user = findById(userId);
+        user.setStatus(UserStatus.ACTIVE);
+        return userRepository.save(user);
     }
 
     @Override
     public void deactivateUser(String userId) {
-
+        User user = findById(userId);
+        user.setStatus(UserStatus.DEACTIVATED);
+        userRepository.save(user);
     }
 
     @Override
     public void deleteUser(String userId) {
+        User user = findById(userId);
+        userRepository.delete(user);
+    }
 
+    private void checkUserRules(Integer dni, String email, String password) {
+        validateDni(dni);
+        validateEmail(email);
+        validatePassword(password);
+    }
+
+    private Integer validateDni(Integer dni) {
+        if ((dni != 0) && userRepository.existsByDni(dni))
+            throw new BadRequestException("Username already in use");
+        return dni;
+    }
+
+    private String validatePassword(String password) {
+        if (password.length() < 8) throw new BadRequestException("Password smaller than 8");
+        // TODO encrypt pwd
+        return password;
+    }
+
+    private String validateEmail(String email) {
+        if ((!email.isEmpty()) && userRepository.existsByEmail(email))
+            throw new BadRequestException("Email already in use");
+        return email;
     }
 }
