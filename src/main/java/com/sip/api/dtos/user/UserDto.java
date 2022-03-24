155425c8026b5117138a096a6c9a556b874f1de5
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 11:55:05 -0300

    user Module: remove conversion from userDto

--- src/main/java/com/sip/api/dtos/user/UserDto.java
+++ src/main/java/com/sip/api/dtos/user/UserDto.java
@@ -24,22 +24,4 @@ public class UserDto {
     private int phone;
     private String zipCode;
     UserStatus status;
-
-    public static UserDto entityToDto(User user) {
-        return UserDto.builder()
-                .dni(user.getDni())
-                .password(user.getPassword())
-                .email(user.getEmail())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .age(user.getAge())
-                .phone(user.getPhone())
-                .zipCode(user.getZipCode())
-                .status(user.getStatus())
-                .build();
-    }
-
-    public static List<UserDto> entityToDto(List<User> users) {
-        return users.stream().map(UserDto::entityToDto).collect(Collectors.toList());
-    }
 }
