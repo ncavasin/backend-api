6791c302244ef1ee7e910a5448a50f5363ee2907
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 13:05:41 -0300

    user Module: remove password from UserDto

--- src/main/java/com/sip/api/dtos/user/UserDto.java
+++ src/main/java/com/sip/api/dtos/user/UserDto.java
@@ -1,14 +1,11 @@
 package com.sip.api.dtos.user;
 
-import com.sip.api.domains.user.User;
 import com.sip.api.domains.enums.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -16,7 +13,6 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class UserDto {
     private int dni;
-    private String password;
     private String email;
     private String firstName;
     private String lastName;
@@ -24,4 +20,5 @@ public class UserDto {
     private int phone;
     private String zipCode;
     UserStatus status;
+
 }
