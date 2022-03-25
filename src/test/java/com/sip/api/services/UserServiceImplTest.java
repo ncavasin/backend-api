cf28996056aa0c4b27ee1443875722a6225c5f7a
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 22:10:01 -0300

    ignore tests as they must be implemented

--- src/test/java/com/sip/api/services/UserServiceImplTest.java
+++ src/test/java/com/sip/api/services/UserServiceImplTest.java
@@ -1,52 +1,51 @@
 package com.sip.api.services;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+
 public class UserServiceImplTest {
 
     @Test
+    @Ignore
     public void shouldCreateUser(){
 
     }
 
     @Test
+    @Ignore
     public void shouldNotCreateWhenWeakPassword(){
 
     }
 
     @Test
-    public void shouldNotCreateWhenDuplicatedUsername(){
-
-    }
-
-    @Test
+    @Ignore
     public void shouldNotCreateWhenDuplicatedEmail(){
 
     }
 
     @Test
-    public void shouldUpdateUsername(){
-
-    }
-
-    @Test
+    @Ignore
     public void shouldUpdateEmail(){
 
     }
 
     @Test
+    @Ignore
     public void shouldUpdatePassword(){
 
     }
 
     @Test
+    @Ignore
     public void shouldDeactivateUser(){
 
     }
 
     @Test
+    @Ignore
     public void shouldDeleteUser(){
 
     }
