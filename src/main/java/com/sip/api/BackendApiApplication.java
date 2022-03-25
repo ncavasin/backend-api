a5af3fa798f08d526c534b20206696a45feb0ff1
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-24 23:43:07 -0300

    remove swagger 2 tag

--- src/main/java/com/sip/api/BackendApiApplication.java
+++ src/main/java/com/sip/api/BackendApiApplication.java
@@ -3,7 +3,6 @@ package com.sip.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaRepositories
