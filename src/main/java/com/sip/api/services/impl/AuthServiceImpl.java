798d720a77ffac918a3ee0c5407d619d78eda4fa
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-31 18:11:14 -0300

    add AuthService & AuthServiceImpl to support AuthController

---
+++ src/main/java/com/sip/api/services/impl/AuthServiceImpl.java
@@ -0,0 +1,40 @@
+package com.sip.api.services.impl;
+
+import com.sip.api.domains.user.User;
+import com.sip.api.dtos.UserCredentialsDto;
+import com.sip.api.dtos.user.UserDniDto;
+import com.sip.api.dtos.user.auth.AuthenticationDto;
+import com.sip.api.exceptions.NotFoundException;
+import com.sip.api.exceptions.UnauthorizedException;
+import com.sip.api.services.AuthService;
+import com.sip.api.services.user.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private final UserService userService;
+
+    @Override
+    public AuthenticationDto authenticate(UserCredentialsDto userCredentialsDto) {
+        // Find user by dni
+        User user;
+        try {
+            user = userService.findByDni(UserDniDto.builder()
+                    .dni(userCredentialsDto.getDni())
+                    .build());
+        } catch (NotFoundException e) {
+            throw new UnauthorizedException("User not found");
+        }
+
+        // Validate password
+
+
+        // Return authentication
+
+        return null;
+    }
+}
