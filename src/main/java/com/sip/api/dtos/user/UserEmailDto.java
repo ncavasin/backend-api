519c8fb20acc806263df36104353d50ee1be448c
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-31 20:30:23 -0300

    add AllArgsConstructor

--- src/main/java/com/sip/api/dtos/user/UserEmailDto.java
+++ src/main/java/com/sip/api/dtos/user/UserEmailDto.java
@@ -1,5 +1,6 @@
 package com.sip.api.dtos.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -8,6 +9,7 @@ import javax.validation.constraints.Email;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserEmailDto {
 
     @NonNull
