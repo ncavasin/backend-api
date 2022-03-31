e5d32f03a02c7615a750720e28805371a7eafa3b
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-31 18:13:56 -0300

    add UserRole entity

---
+++ src/main/java/com/sip/api/domains/user/UserRole.java
@@ -0,0 +1,24 @@
+package com.sip.api.domains.user;
+
+import com.sip.api.domains.AbstractEntity;
+import com.sip.api.domains.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_role")
+public class UserRole extends AbstractEntity {
+    @ManyToOne
+    private User user;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+}
