d571cc4fcb9363880de7302ff0ae939c3239ea53
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-01 19:43:01 -0300

    role-based-auth: linter spacing

--- src/main/java/com/sip/api/services/impl/RegisterServiceImpl.java
+++ src/main/java/com/sip/api/services/impl/RegisterServiceImpl.java
@@ -27,9 +27,9 @@ public class RegisterServiceImpl implements RegisterService {
         // TODO: handle resend confirmation mail
         String token = mailTokenService.createTokenForUser(savedUser);
         String link = "http://localhost:8080/register/confirm?token=" + token;
-        try{
+        try {
             mailSender.sendMail(savedUser.getEmail(), "Geminis - Activate your account!", buildEmail(savedUser.getFirstName(), link));
-        }catch (Exception e){
+        } catch (Exception e) {
             log.error("Error sending mail: {}.", e.getMessage());
             e.printStackTrace();
         }
@@ -45,7 +45,7 @@ public class RegisterServiceImpl implements RegisterService {
     }
 
     private String buildEmail(String userFirstName, String link) {
-        return  "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
+        return "<div style=\"font-family:Helvetica,Arial,sans-serif;font-size:16px;margin:0;color:#0b0c0c\">\n" +
                 "\n" +
                 "<span style=\"display:none;font-size:1px;color:#fff;max-height:0\"></span>\n" +
                 "\n" +
