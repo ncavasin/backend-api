815aa4f2c74555cb94ed29b77fb9d6ae69020cd3
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-31 22:13:14 -0300

    add join column with user

--- src/main/java/com/sip/api/domains/user/UserRole.java
+++ src/main/java/com/sip/api/domains/user/UserRole.java
@@ -17,6 +17,7 @@ import javax.persistence.*;
 @Table(name = "user_role")
 public class UserRole extends AbstractEntity {
     @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @Enumerated(value = EnumType.STRING)
