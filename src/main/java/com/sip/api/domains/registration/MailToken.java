2106ede03b10b0a8d3c820da580ce2981f4eac16
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-01 00:39:52 -0300

    add MailToken entity to support activation by mail

---
+++ src/main/java/com/sip/api/domains/registration/MailToken.java
@@ -0,0 +1,33 @@
+package com.sip.api.domains.registration;
+
+import com.sip.api.domains.AbstractEntity;
+import com.sip.api.domains.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "mail_token")
+public class MailToken extends AbstractEntity {
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Timestamp createdAt;
+
+    @Column(nullable = false)
+    private Timestamp expiresAt;
+
+    private Timestamp confirmedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
+
