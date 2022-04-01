bc2c084f5a411f823f11bed1cb4cc3268ebd9433
Nicolas Cavasin <ncavasin97@gmail.com>
2022-03-31 21:31:01 -0300

    improve uuid definition

--- src/main/java/com/sip/api/domains/AbstractEntity.java
+++ src/main/java/com/sip/api/domains/AbstractEntity.java
@@ -11,8 +11,8 @@ import javax.persistence.*;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "uuid_user_data_generator")
-    @GenericGenerator(name = "uuid_user_data_generator", strategy = "uuid2")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator = "uuid_generator")
+    @GenericGenerator(name = "uuid_generator", strategy = "uuid2")
     protected String id;
 
 }
