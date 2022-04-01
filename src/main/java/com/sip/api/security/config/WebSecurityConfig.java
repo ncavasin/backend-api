c0466ff93d4b310bdfa20ef34c3ceec6c7308c83
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-01 18:04:37 -0300

    formatting

--- src/main/java/com/sip/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/sip/api/security/config/WebSecurityConfig.java
@@ -22,13 +22,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().disable()
+                .csrf().and().cors().disable()
                 .authorizeRequests()
                 .antMatchers("/register/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated().and()
+                .authenticated()
+                .and()
                 .formLogin();
     }
 
