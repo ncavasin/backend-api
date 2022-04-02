9696fcd46f88856e4c5df49673cf5395769034b7
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-02 16:27:04 -0300

    SIP27: add R-U-D endpoints for roles

--- src/main/java/com/sip/api/controllers/ManagementController.java
+++ src/main/java/com/sip/api/controllers/ManagementController.java
@@ -9,10 +9,9 @@ import com.sip.api.services.RoleService;
 import com.sip.api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/management")
@@ -21,10 +20,32 @@ public class ManagementController {
     private final UserService userService;
     private final RoleService roleService;
 
-    @PostMapping("/role")
-    public Role addRole(@RequestBody @Validated String roleName) {
-        return roleService. createRole(roleName);
+    @GetMapping("/role")
+    public RoleDto getRole(@RequestBody @Validated RoleDto roleDto) {
+        return RoleConverter.entityToDto(roleService.findByName(roleDto.getName()));
     }
+
+    @GetMapping("/role")
+    public List<RoleDto> getAll(){
+        return RoleConverter.entityToDto(roleService.findAll());
+    }
+
+    @PostMapping("/role")
+    public RoleDto addRole(@RequestBody @Validated RoleDto roleDto) {
+        return RoleConverter.entityToDto(roleService. createRole(roleDto.getName()));
+    }
+
+    @PutMapping("/role")
+    public RoleDto updateRoleName(@RequestBody @Validated RoleDto roleDto) {
+        return RoleConverter.entityToDto(roleService.updateName(roleDto.getId(), roleDto.getName()));
+    }
+
+    @DeleteMapping
+    public void deleteRole(@RequestBody @Validated RoleDto roleDto) {
+        roleService.deleteRole(roleDto.getId());
+    }
+
+
 
     @PostMapping("/add-professor")
     public UserDto addProfessor(@RequestBody @Validated UserCreationDto userCreationDto) {
