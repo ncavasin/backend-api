7e22b6fa90e853417d922cec433502368e7066c4
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-04 23:29:47 -0300

    SIP-27: add Resources entity and map unidirectionally ManyToMany with Roles, being Roles the owner of the relation

---
+++ src/main/java/com/sip/api/domains/resource/Resources.java
@@ -0,0 +1,24 @@
+package com.sip.api.domains.resource;
+
+
+import com.sip.api.domains.TimeTrackable;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "resources")
+public class Resources extends TimeTrackable {
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String url;
+
+}
--- src/main/java/com/sip/api/domains/role/Role.java
+++ src/main/java/com/sip/api/domains/role/Role.java
@@ -1,14 +1,17 @@
 package com.sip.api.domains.role;
 
 import com.sip.api.domains.TimeTrackable;
+import com.sip.api.domains.resource.Resources;
 import lombok.*;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.NaturalId;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -21,6 +24,13 @@ public class Role extends TimeTrackable {
     @Column(nullable = false, unique = true)
     @NaturalId
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "roles_resources",
+            joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_role_id")),
+            inverseJoinColumns = @JoinColumn(name = "resource_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_resource_id")))
+    @Fetch(FetchMode.JOIN)
+    Set<Resources> allowedResources = new LinkedHashSet<>();
 
     @Override
     public boolean equals(Object o) {
