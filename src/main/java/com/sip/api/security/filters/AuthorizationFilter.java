8594dabaf0cacc5d3e34c91f592c70c0abcd8f09
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-07 00:03:29 -0300

    SIP-27: update AuthenticationFilter to interact with userService & coder is cleaner now

--- src/main/java/com/sip/api/security/filters/AuthorizationFilter.java
+++ src/main/java/com/sip/api/security/filters/AuthorizationFilter.java
@@ -1,9 +1,16 @@
 package com.sip.api.security.filters;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.common.base.Strings;
+import com.sip.api.domains.user.User;
+import com.sip.api.dtos.user.UserEmailDto;
+import com.sip.api.services.UserService;
 import com.sip.api.utils.JwtFactory;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,37 +23,57 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.stream.Stream;
 
 @Slf4j
+@RequiredArgsConstructor
 public class AuthorizationFilter extends OncePerRequestFilter {
+    private final String BEARER_AND_SPACE = "Bearer ";
+    private final UserService userService;
+
+    /*
+     * This filter extracts the token from the request and validates its roles & permissions.
+     * User authentication is managed in a previous filter.
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
-        // Skip login
-        if (!request.getServletPath().equals("/login") || !request.getServletPath().contains("swagger")) {
-            String authHeader = request.getHeader("Authorization");
-            try {
-                // Skip if token is missing or isn't a jwt bearer
-                if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                    DecodedJWT decodedToken = JwtFactory.decodeToken(authHeader.replace("Bearer ", ""));
-                    // Skip if token decoding failed
-                    if (decodedToken != null) {
-                        String username = decodedToken.getSubject();
-                        String[] roles = decodedToken.getClaim("roles").asArray(String.class);
 
-                        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-                        Stream.of(roles).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
 
-                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
-                        SecurityContextHolder.getContext().setAuthentication(authentication);
-                    }
-                }
-            } catch (Exception e) {
-                log.error("Error while verifying JWT token. Error: {}.", e.getMessage());
-            }
+        // If the request does not contain an authorization header, it won't be authorized but must still traverse the filter chain
+        // because it might try access public resources.
+        if (Strings.isNullOrEmpty(authHeader) || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
+            return;
         }
+
+        // Try to decode token
+        DecodedJWT decodedToken = JwtFactory.decodeToken(authHeader.replace(BEARER_AND_SPACE, ""));
+
+        // If decoding fails, it means the token is invalid
+        if (decodedToken == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Decoding succeeded. Now fetch user from database using the email received in the token
+        String username = decodedToken.getSubject();
+        User user = userService.findByEmail(UserEmailDto.builder().email(username).build());
+
+        // Get roles from the token and convert them to Spring Security authorities
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        // Finally, verify that the user has the resource permission to access the url
+        String requestingUrl = request.getRequestURL().toString();
+        HttpMethod requestingMethod = HttpMethod.valueOf(request.getMethod());
+
+        // TODO: add query to fetch allowed resources
+
+        // Now return the roles the user has as authorities
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
     }
 }
