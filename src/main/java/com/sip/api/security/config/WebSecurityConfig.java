97f8e22d7e230e5370be1c47b6bb6e5783583afc
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-07 00:26:39 -0300

    SIP-27: improve declaration to make it more verbose

--- src/main/java/com/sip/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/sip/api/security/config/WebSecurityConfig.java
@@ -3,23 +3,22 @@ package com.sip.api.security.config;
 import com.sip.api.security.filters.AuthenticationFilter;
 import com.sip.api.security.filters.AuthorizationFilter;
 import com.sip.api.services.UserService;
+import com.sip.api.utils.JwtHandlerUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Configuration
-@RequiredArgsConstructor
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
+    private final JwtHandlerUtil jwtHandlerUtil;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.csrf()
                 .disable()
                 .cors()
@@ -36,13 +35,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/login/**", "/register/**", "/password/**")
                 .permitAll();
         http
-                .addFilter(new AuthenticationFilter(authenticationManager()))
-                .addFilterAfter(new AuthorizationFilter(userService), AuthenticationFilter.class)
-//            .addFilterBefore(new AuthorizationFilter(userService), UsernamePasswordAuthenticationFilter.class)
-//            .authorizeRequests()
-//                .antMatchers("/logout/**", "/management/**")//, "/users/**")
-//                .permitAll()
-                .authorizeRequests()
+                .addFilter(new AuthenticationFilter(authenticationManager(), jwtHandlerUtil))
+                .addFilterAfter(new AuthorizationFilter(userService, jwtHandlerUtil), AuthenticationFilter.class);
+
+        http.authorizeRequests()
                 .anyRequest()
                 .authenticated();
     }
