ec4867b575813661dec4bc9321b863b5323fbc2d
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-07 22:19:00 -0300

    SIP-27: spacing

--- src/main/java/com/sip/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/sip/api/security/config/WebSecurityConfig.java
@@ -19,7 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf()
+        http
+                .csrf()
                 .disable()
                 .cors()
                 .disable();
