02123f23e30befb70fe0144c9696d13269214485
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-10 11:42:49 -0300

    SIP28: embed allowed resources in the token issuance

--- src/main/java/com/sip/api/services/impl/JwtServiceImpl.java
+++ src/main/java/com/sip/api/services/impl/JwtServiceImpl.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sip.api.domains.role.Role;
+import com.sip.api.exceptions.UnauthorizedException;
 import com.sip.api.services.JwtService;
 import com.sip.api.services.RoleService;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ import org.springframework.stereotype.Component;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -36,41 +37,21 @@ public class JwtServiceImpl implements JwtService {
 
     @Override
     public String issueAuthToken(User user) {
-        String jwt = String.format("Error issuing authentication token for user: %s. Message: %s", user.getUsername(), "Not implemented");
         try {
-            jwt = JWT.create()
+            String jwt = JWT.create()
                     .withSubject(user.getUsername())
                     .withIssuedAt(getIssuingDate())
                     .withExpiresAt(getExpirationDate())
                     .withIssuer(issuer)
 //                    .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
-                    .withClaim("roles", getResourcesByRoleName(user))
+                    .withClaim("roles", getResourcesByRoleName(user).stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                     .sign(getSigningAlgorithm());
             log.info("New jot. Issuer {}. Algorithm {}. Token {}", issuer, getSigningAlgorithm(), jwt);
+            return jwt;
         } catch (Exception e) {
             log.error("Error issuing auth token for user: {}. {}", user.getUsername(), e.getMessage());
+            throw new UnauthorizedException("Error issuing auth token for user: " + user.getUsername() + ". " + e.getMessage());
         }
-        return jwt;
-    }
-
-    private List<SimpleGrantedAuthority> getResourcesByRoleName(UserDetails userDetails){
-        List<SimpleGrantedAuthority> rolesAndResources = new ArrayList<>();
-
-        // Get user roles through UserDetails interface as Collection<GrantedAuthority> (collection of roles names)
-        userDetails.getAuthorities().forEach(authority -> {
-            // TODO: FINISH THIS!!!!
-            // Get role by name
-            Role role = roleService.findByName(authority.getAuthority());
-            // For each role get its allowed resources
-            role.getAllowedResources().forEach(resource -> {
-                // For each resource parse it as a String
-//                String prueba = resource.getName() + "|" + resource.getUrl() + "|" ;
-                // And add it to the list of roles and resources
-                rolesAndResources.add(new SimpleGrantedAuthority(resource.toString()));
-                // IT WORKS BUT I NEED TO IMPLEMENT THE CREATION OF THE RESOURCE ASSOCIATED TO A ROLE
-            });
-        });
-        return rolesAndResources;
     }
 
     @Override
@@ -98,6 +79,27 @@ public class JwtServiceImpl implements JwtService {
             log.error("Error decoding invalid token: {}", e.getMessage());
         }
         return null;
+    }
+
+    /**
+     * Here lays the magic:
+     * Through the UserDetails interface we can get the user's roles since they're returned as a collection of GrantedAuthorities.
+     * For every GrantedAuthority (aka Role) use the roleService to fetch the role from the database.
+     * With fetched role, iterate over its resources and add them to the collection of GrantedAuthorities.
+     *
+     * @param userDetails Spring security interface that allows fetching the user Roles.
+     * @return a list of GrantedAuthorities that represent the resources the user is allowed to get.
+     */
+    private Collection<GrantedAuthority> getResourcesByRoleName(UserDetails userDetails) {
+        Collection<GrantedAuthority> rolesAndResources = new ArrayList<>();
+        // Get user roles through UserDetails interface as Collection<GrantedAuthority> (collection of roles names)
+        userDetails.getAuthorities().forEach(authority -> {
+            // Get role by name
+            Role role = roleService.findByName(authority.getAuthority());
+            // For each role get its allowed resources and set them as granted authorities
+            role.getAllowedResources().forEach(resource -> rolesAndResources.add(new SimpleGrantedAuthority(resource.getUrl())));
+        });
+        return rolesAndResources;
     }
 
     @Override
