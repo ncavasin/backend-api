ef26ee25d717a1a72abc011bfbbb9b976cc043b7
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-10 12:52:12 -0300

    SIP28: add filtering by JWT claims in AuthorizationFilter

--- src/main/java/com/sip/api/security/filters/AuthorizationFilter.java
+++ src/main/java/com/sip/api/security/filters/AuthorizationFilter.java
@@ -2,9 +2,7 @@ package com.sip.api.security.filters;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.base.Strings;
-import com.sip.api.domains.user.User;
-import com.sip.api.dtos.user.UserEmailDto;
-import com.sip.api.services.UserService;
+import com.sip.api.exceptions.ForbiddenException;
 import com.sip.api.services.JwtService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,14 +19,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Slf4j
 @RequiredArgsConstructor
 public class AuthorizationFilter extends OncePerRequestFilter {
     private final String BEARER_AND_SPACE = "Bearer ";
-    private final UserService userService;
     private final JwtService jwtService;
 
     /*
@@ -74,22 +70,27 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             return;
         }
 
-        // Decoding succeeded. Now fetch user from database using the email received in the token
-        String username = jwtService.getSubject(decodedToken);
-        User user = userService.findByEmail(UserEmailDto.builder().email(username).build());
-
         // Get roles from the token and convert them to Spring Security authorities
-        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
-        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        Collection<GrantedAuthority> allowedResources = jwtService.getAllowedResourcesAsGrantedAuthorities(decodedToken);
 
         // Finally, verify that the user has the resource permission to access the url
         String requestingUrl = request.getRequestURL().toString();
-        HttpMethod requestingMethod = HttpMethod.valueOf(request.getMethod());
 
-        // TODO: add query to fetch allowed resources
+        // TODO: FILTER BY HTTP METHOD TOO
+        HttpMethod requestingMethod = HttpMethod.valueOf(request.getMethod());
+        boolean hasAccessToEverything = allowedResources.stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().contains("/*"));
+        boolean hasAccessToUrl = allowedResources.stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(requestingUrl));
+
+//        Collection<GrantedAuthority> matches = allowedResources.stream().filter(grantedAuthority -> grantedAuthority.getAuthority().contains(requestingUrl)).collect(Collectors.toList());
+
+        if (hasAccessToEverything | hasAccessToUrl){
+            log.info("User does not have permission to access {}.", requestingUrl);
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         // Now return the roles the user has as authorities
-        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(jwtService.getSubject(decodedToken), null, allowedResources);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
