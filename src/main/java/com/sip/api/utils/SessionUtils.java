819337137241fcafd95adf5ba20473aff8e5b940
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-17 13:19:26 -0300

    spacing

--- src/main/java/com/sip/api/utils/SessionUtils.java
+++ src/main/java/com/sip/api/utils/SessionUtils.java
@@ -4,9 +4,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
-public  class SessionUtils {
+public class SessionUtils {
 
-    public static String getLoggedUserUsername(){
+    public static String getLoggedUserUsername() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
