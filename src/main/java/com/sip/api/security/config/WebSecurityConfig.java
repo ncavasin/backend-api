842e321e5227e8a97491440379880b3653389a07
Nicolas Cavasin <ncavasin97@gmail.com>
2022-04-22 16:08:36 -0300

    move filters down

--- src/main/java/com/sip/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/sip/api/security/config/WebSecurityConfig.java
@@ -28,15 +28,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable();
 
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http
-                .addFilter(new AuthenticationFilter(authenticationManager(), jwtService))
-                .addFilterAfter(new AuthorizationFilter(jwtService), AuthenticationFilter.class);
         // public endpoints
         http.authorizeRequests()
                 .antMatchers("/api-docs/**", "/swagger-ui**" , "/swagger-ui/**", "/swagger-ui/index.html",
                         "/login/**", "/register/**", "/password/**")
                 .permitAll();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http
+                .addFilter(new AuthenticationFilter(authenticationManager(), jwtService))
+                .addFilterAfter(new AuthorizationFilter(jwtService), AuthenticationFilter.class);
 
         http.authorizeRequests()
                 .anyRequest()
