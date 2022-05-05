075a08df9d5844a57b67c6eb211c83317144a4a3
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-05 18:31:49 -0300

    SIP12: update ActivityCreationDto to be a Set of Appointments instead of its IDs

--- src/main/java/com/sip/api/dtos/activity/ActivityCreationDto.java
+++ src/main/java/com/sip/api/dtos/activity/ActivityCreationDto.java
@@ -1,5 +1,6 @@
 package com.sip.api.dtos.activity;
 
+import com.sip.api.domains.appointment.Appointment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +15,5 @@ public class ActivityCreationDto {
     private String name;
     @NonNegative
     private Double basePrice;
-    private Set<String> appointmentIds;
+    private Set<Appointment> appointments;
 }
