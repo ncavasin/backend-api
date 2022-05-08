205b01460abc8694d12ecd4e8a047e748ea94b37
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-08 16:55:28 -0300

    SIP65: update Reservation FK definition

--- src/main/java/com/sip/api/domains/reservation/Reservation.java
+++ src/main/java/com/sip/api/domains/reservation/Reservation.java
@@ -21,7 +21,7 @@ import java.util.Set;
 @Table(name = "reservation")
 public class Reservation extends TimeTrackable {
     @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "availableClass_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_availableClass_reservation_id"))
+    @JoinColumn(name = "available_class_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "FK_available_class_reservation_id"))
     @Fetch(FetchMode.JOIN)
     private Set<AvailableClass> availableClasses = new java.util.LinkedHashSet<>();
 
