164b5381491f014decc3fdf4408e1d446ab922dc
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-10 18:49:17 -0300

    SIP65: define ReservationCreationDto

--- src/main/java/com/sip/api/dtos/reservation/ReservationCreationDto.java
+++ src/main/java/com/sip/api/dtos/reservation/ReservationCreationDto.java
@@ -1,4 +1,19 @@
 package com.sip.api.dtos.reservation;
 
+import com.sip.api.domains.availableClass.AvailableClass;
+import com.sip.api.domains.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationCreationDto {
+    private Set<AvailableClass> availableClasses;
+    private Set<User> ateendees;
 }
