20e37bdaa444ca25695e9a305d4bdcabfcb71ef8
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-10 18:32:42 -0300

    SIP65: adding not null constraint to Timeslot dtos

--- src/main/java/com/sip/api/dtos/timeslot/TimeslotCreationDto.java
+++ src/main/java/com/sip/api/dtos/timeslot/TimeslotCreationDto.java
@@ -4,6 +4,7 @@ import com.sip.api.domains.availableClass.AvailableClass;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.Set;
@@ -11,8 +12,11 @@ import java.util.Set;
 @Data
 @Builder
 public class TimeslotCreationDto {
+    @NotNull
     private LocalTime startTime;
+    @NotNull
     private LocalTime endTime;
+    @NotNull
     private DayOfWeek dayOfWeek;
     private Set<AvailableClass> availableClasses;
 }
--- src/main/java/com/sip/api/dtos/timeslot/TimeslotDto.java
+++ src/main/java/com/sip/api/dtos/timeslot/TimeslotDto.java
@@ -3,6 +3,7 @@ package com.sip.api.dtos.timeslot;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
@@ -10,7 +11,10 @@ import java.time.LocalTime;
 @Builder
 public class TimeslotDto {
     private String id;
+    @NotNull
     private LocalTime startTime;
+    @NotNull
     private LocalTime endTime;
+    @NotNull
     private DayOfWeek dayOfWeek;
 }
