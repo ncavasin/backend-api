4c48bc1f0975b146809308ade83f08086004d71a
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-10 22:56:36 -0300

    SIP65: adjust converter to new dto mappings

--- src/main/java/com/sip/api/domains/reservation/ReservationConverter.java
+++ src/main/java/com/sip/api/domains/reservation/ReservationConverter.java
@@ -1,6 +1,7 @@
 package com.sip.api.domains.reservation;
 
 import com.sip.api.dtos.reservation.ReservationDto;
+import com.sip.api.dtos.user.UserSlimDto;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +16,15 @@ public class ReservationConverter {
     public static ReservationDto fromEntityToDto(Reservation reservation) {
         return ReservationDto.builder()
                 .id(reservation.getId())
-
+                .availableClass(reservation.getAvailableClass())
+                .attendees(reservation.getAttendees().stream()
+                        .map(attendee -> UserSlimDto.builder()
+                                .id(attendee.getId())
+                                .email(attendee.getEmail())
+                                .firstName(attendee.getFirstName())
+                                .lastName(attendee.getLastName())
+                                .build())
+                        .collect(Collectors.toSet()))
                 .build();
     }
 }
