d1012102a4c42e4b2a638e71de9d755270bfcc9e
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 00:55:38 -0300

    SIP65: add endpoints for taking and leaving a slot in AvailableClass

--- src/main/java/com/sip/api/controllers/AvailableClassController.java
+++ src/main/java/com/sip/api/controllers/AvailableClassController.java
@@ -31,6 +31,16 @@ public class AvailableClassController {
         return AvailableClassConverter.fromEntityToDto(availableClassService.createAvailableClass(availableClassesCreationDto));
     }
 
+    @PutMapping("/{availableClassId}/add-user/{userId}")
+    public AvailableClassDto addUserToAvailableClass(@PathVariable("availableClassId") String availableClassId, @PathVariable("userId") String userId) {
+        return AvailableClassConverter.fromEntityToDto(availableClassService.addUserToAvailableClass(availableClassId, userId));
+    }
+
+    @PutMapping("/{availableClassId}/remove-user/{userId}")
+    public AvailableClassDto removeUserFromAvailableClass(@PathVariable("availableClassId") String availableClassId, @PathVariable("userId") String userId) {
+        return AvailableClassConverter.fromEntityToDto(availableClassService.removeUserFromAvailableClass(availableClassId, userId));
+    }
+
     @DeleteMapping("/{availableClassId}")
     public void removeAvailableClassById(@PathVariable("availableClassId") String availableClassId) {
         availableClassService.removeAvailableClass(availableClassId);
