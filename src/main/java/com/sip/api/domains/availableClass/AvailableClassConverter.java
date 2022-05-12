6f15783ae6a051591a40c7a2657779e057dbbbe1
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 00:57:34 -0300

    SIP65: adjust converter to match AvailableClassDto modifications

--- src/main/java/com/sip/api/domains/availableClass/AvailableClassConverter.java
+++ src/main/java/com/sip/api/domains/availableClass/AvailableClassConverter.java
@@ -1,9 +1,14 @@
 package com.sip.api.domains.availableClass;
 
+import com.sip.api.domains.user.User;
+import com.sip.api.dtos.activity.ActivityDto;
 import com.sip.api.dtos.availableClass.AvailableClassDto;
+import com.sip.api.dtos.timeslot.TimeslotDto;
+import com.sip.api.dtos.user.UserSlimDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -18,8 +23,35 @@ public class AvailableClassConverter {
     public static AvailableClassDto fromEntityToDto(AvailableClass availableClass) {
         return AvailableClassDto.builder()
                 .id(availableClass.getId())
-                .activity(availableClass.getActivity())
-                .timeslot(availableClass.getTimeslot())
+                .activityDto(ActivityDto.builder()
+                        .id(availableClass.getActivity().getId())
+                        .name(availableClass.getActivity().getName())
+                        .atendeesLimit(availableClass.getActivity().getAtendeesLimit())
+                        .basePrice(availableClass.getActivity().getBasePrice())
+                        .build())
+                .timeslotDto(TimeslotDto.builder()
+                        .id(availableClass.getTimeslot().getId())
+                        .startTime(availableClass.getTimeslot().getStartTime())
+                        .endTime(availableClass.getTimeslot().getEndTime())
+                        .dayOfWeek(availableClass.getTimeslot().getDayOfWeek())
+                        .build())
+                .attendees(mapToSlimUserDtoSet(availableClass.getAttendees()))
+                .rejectedAttendees(mapToSlimUserDtoSet(availableClass.getRejectedAttendees()))
+                .build();
+    }
+
+    private static Set<UserSlimDto> mapToSlimUserDtoSet(Set<User> attendees) {
+        return attendees.stream()
+                .map(AvailableClassConverter::mapToSlimUserDto)
+                .collect(Collectors.toSet());
+    }
+
+    private static UserSlimDto mapToSlimUserDto(User attendee) {
+        return UserSlimDto.builder()
+                .id(attendee.getId())
+                .firstName(attendee.getFirstName())
+                .lastName(attendee.getLastName())
+                .email(attendee.getEmail())
                 .build();
     }
 }
