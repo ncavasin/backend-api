22c618feca4d252000a389a6940a514835fa7830
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 00:56:35 -0300

    SIP65: adjust AvailableClassDto to be smaller

--- src/main/java/com/sip/api/dtos/availableClass/AvailableClassDto.java
+++ src/main/java/com/sip/api/dtos/availableClass/AvailableClassDto.java
@@ -2,12 +2,17 @@ package com.sip.api.dtos.availableClass;
 
 import com.sip.api.domains.activity.Activity;
 import com.sip.api.domains.timeslot.Timeslot;
+import com.sip.api.domains.user.User;
+import com.sip.api.dtos.activity.ActivityDto;
+import com.sip.api.dtos.timeslot.TimeslotDto;
+import com.sip.api.dtos.user.UserSlimDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Data
 @Builder
@@ -17,7 +22,9 @@ public class AvailableClassDto {
     @NotNull
     private String id;
     @NotNull
-    private Activity activity;
+    private ActivityDto activityDto;
     @NotNull
-    private Timeslot timeslot;
+    private TimeslotDto timeslotDto;
+    private Set<UserSlimDto> attendees;
+    private Set<UserSlimDto> rejectedAttendees;
 }
