6411791ffa948e3f170d8ac04e69bf9431bf0199
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-12 00:54:58 -0300

    SIP65: COMMENT EVERYTHING RELATED TO Reservation. May revert later when improving mapping.

--- src/main/java/com/sip/api/domains/reservation/ReservationConverter.java
+++ src/main/java/com/sip/api/domains/reservation/ReservationConverter.java
@@ -1,30 +1,30 @@
-package com.sip.api.domains.reservation;
-
-import com.sip.api.dtos.reservation.ReservationDto;
-import com.sip.api.dtos.user.UserSlimDto;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class ReservationConverter {
-    public static List<ReservationDto> fromEntityToDto(List<Reservation> reservations) {
-        return reservations.stream()
-                .map(ReservationConverter::fromEntityToDto)
-                .collect(Collectors.toList());
-    }
-
-    public static ReservationDto fromEntityToDto(Reservation reservation) {
-        return ReservationDto.builder()
-                .id(reservation.getId())
-                .availableClass(reservation.getAvailableClass())
-                .attendees(reservation.getAttendees().stream()
-                        .map(attendee -> UserSlimDto.builder()
-                                .id(attendee.getId())
-                                .email(attendee.getEmail())
-                                .firstName(attendee.getFirstName())
-                                .lastName(attendee.getLastName())
-                                .build())
-                        .collect(Collectors.toSet()))
-                .build();
-    }
-}
+//package com.sip.api.domains.reservation;
+//
+//import com.sip.api.dtos.reservation.ReservationDto;
+//import com.sip.api.dtos.user.UserSlimDto;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//public class ReservationConverter {
+//    public static List<ReservationDto> fromEntityToDto(List<Reservation> reservations) {
+//        return reservations.stream()
+//                .map(ReservationConverter::fromEntityToDto)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public static ReservationDto fromEntityToDto(Reservation reservation) {
+//        return ReservationDto.builder()
+//                .id(reservation.getId())
+//                .availableClass(reservation.getAvailableClass())
+//                .attendees(reservation.getAttendees().stream()
+//                        .map(attendee -> UserSlimDto.builder()
+//                                .id(attendee.getId())
+//                                .email(attendee.getEmail())
+//                                .firstName(attendee.getFirstName())
+//                                .lastName(attendee.getLastName())
+//                                .build())
+//                        .collect(Collectors.toSet()))
+//                .build();
+//    }
+//}
--- src/main/java/com/sip/api/services/ReservationService.java
+++ src/main/java/com/sip/api/services/ReservationService.java
@@ -1,15 +1,15 @@
-package com.sip.api.services;
-
-import com.sip.api.domains.reservation.Reservation;
-import com.sip.api.dtos.reservation.ReservationCreationDto;
-
-import java.util.List;
-
-public interface ReservationService {
-    List<Reservation> findAll();
-
-    Reservation findById(String reservationId);
-
-    Reservation createReservation(ReservationCreationDto reservationCreationDto);
-    void deleteReservation(String reservationId);
-}
+//package com.sip.api.services;
+//
+//import com.sip.api.domains.reservation.Reservation;
+//import com.sip.api.dtos.reservation.ReservationCreationDto;
+//
+//import java.util.List;
+//
+//public interface ReservationService {
+//    List<Reservation> findAll();
+//
+//    Reservation findById(String reservationId);
+//
+//    Reservation createReservation(ReservationCreationDto reservationCreationDto);
+//    void deleteReservation(String reservationId);
+//}
