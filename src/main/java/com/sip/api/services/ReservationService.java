e9ac4b8ab8441bad7a72f3fbdb904e018f69b5e4
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-15 18:58:26 -0300

    SIP65: add Reservation service + impl

--- src/main/java/com/sip/api/services/ReservationService.java
+++ src/main/java/com/sip/api/services/ReservationService.java
@@ -1,15 +1,18 @@
-//package com.sip.api.services;
-//
-//import com.sip.api.domains.reservation.Reservation;
-//import com.sip.api.dtos.reservation.ReservationCreationDto;
-//
-//import java.util.List;
-//
-//public interface ReservationService {
-//    List<Reservation> findAll();
-//
-//    Reservation findById(String reservationId);
-//
-//    Reservation createReservation(ReservationCreationDto reservationCreationDto);
-//    void deleteReservation(String reservationId);
-//}
+package com.sip.api.services;
+
+import com.sip.api.domains.reservation.Reservation;
+import com.sip.api.dtos.reservation.ReservationCreationDto;
+
+import java.util.List;
+
+public interface ReservationService {
+    List<Reservation> findAll();
+
+    Reservation findById(String reservationId);
+
+    Reservation addUserToReservation(ReservationCreationDto reservationCreationDto);
+
+    Reservation removeUserFromReservation(String reservationId, String attendeeId);
+
+    void deleteReservation(String reservationId);
+}
