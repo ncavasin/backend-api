dfc857abb0c6afe2a92a182c94c0ec4f404d606e
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-15 18:57:45 -0300

    SIP65: add Reservation controller + dtos & converter

--- src/main/java/com/sip/api/controllers/ReservationController.java
+++ src/main/java/com/sip/api/controllers/ReservationController.java
@@ -1,37 +1,42 @@
-//package com.sip.api.controllers;
-//
-//import com.sip.api.domains.reservation.ReservationConverter;
-//import com.sip.api.dtos.reservation.ReservationCreationDto;
-//import com.sip.api.dtos.reservation.ReservationDto;
-//import com.sip.api.services.ReservationService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/reservation")
-//@RequiredArgsConstructor
-//public class ReservationController {
-//    private final ReservationService reservationService;
-//
-//    @GetMapping("/all")
-//    public List<ReservationDto> findAll() {
-//        return ReservationConverter.fromEntityToDto(reservationService.findAll());
-//    }
-//
-//    @GetMapping("/{reservationId}")
-//    public ReservationDto findById(@PathVariable("reservationId") String reservationId) {
-//        return ReservationConverter.fromEntityToDto(reservationService.findById(reservationId));
-//    }
-//
-//    @PostMapping
-//    public ReservationDto createReservation(@RequestBody ReservationCreationDto reservationCreationDto) {
-//        return ReservationConverter.fromEntityToDto(reservationService.createReservation(reservationCreationDto));
-//    }
-//
-//    @DeleteMapping("/{reservationId}")
-//    public void deleteReservation(@PathVariable("reservationId") String reservationId) {
-//        reservationService.deleteReservation(reservationId);
-//    }
-//}
+package com.sip.api.controllers;
+
+import com.sip.api.domains.reservation.ReservationConverter;
+import com.sip.api.dtos.reservation.ReservationCreationDto;
+import com.sip.api.dtos.reservation.ReservationDto;
+import com.sip.api.services.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservation")
+@RequiredArgsConstructor
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @GetMapping("/all")
+    public List<ReservationDto> findAll() {
+        return ReservationConverter.fromEntityToDto(reservationService.findAll());
+    }
+
+    @GetMapping("/{reservationId}")
+    public ReservationDto findById(@PathVariable("reservationId") String reservationId) {
+        return ReservationConverter.fromEntityToDto(reservationService.findById(reservationId));
+    }
+
+    @PostMapping()
+    public ReservationDto addUserToReservation(@RequestBody ReservationCreationDto reservationCreationDto) {
+        return ReservationConverter.fromEntityToDto(reservationService.addUserToReservation(reservationCreationDto));
+    }
+
+    @PutMapping("/{reservationId}/{attendeeId}")
+    public ReservationDto removeUserFromReservation(@PathVariable("reservationId") String reservationId, @PathVariable("attendeeId") String attendeeId) {
+        return ReservationConverter.fromEntityToDto(reservationService.removeUserFromReservation(reservationId, attendeeId));
+    }
+
+    @DeleteMapping("/{reservationId}")
+    public void deleteReservation(@PathVariable("reservationId") String reservationId) {
+        reservationService.deleteReservation(reservationId);
+    }
+}
