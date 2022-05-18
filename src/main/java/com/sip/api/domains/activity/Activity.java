4cde7f532003e9c20add493e88838c84aad25893
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-18 19:04:56 -0300

    fix: make professor fetching eagerly

--- src/main/java/com/sip/api/domains/activity/Activity.java
+++ src/main/java/com/sip/api/domains/activity/Activity.java
@@ -24,7 +24,7 @@ public class Activity extends TimeTrackable {
     @Column(nullable = false)
     private Double basePrice;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User professor;
 
