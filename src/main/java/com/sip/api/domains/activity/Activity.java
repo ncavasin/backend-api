9122ed1ed5a4a9c596e72a53d35b30e894b0ca36
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-18 17:58:36 -0300

    fix: add professor, equals & hashCode to Activity

--- src/main/java/com/sip/api/domains/activity/Activity.java
+++ src/main/java/com/sip/api/domains/activity/Activity.java
@@ -1,12 +1,13 @@
 package com.sip.api.domains.activity;
 
 import com.sip.api.domains.TimeTrackable;
+import com.sip.api.domains.user.User;
 import lombok.*;
 import org.checkerframework.checker.index.qual.NonNegative;
+import org.hibernate.Hibernate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -23,6 +24,23 @@ public class Activity extends TimeTrackable {
     @Column(nullable = false)
     private Double basePrice;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User professor;
+
     @NonNegative
     private int attendeesLimit;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Activity activity = (Activity) o;
+        return id != null && Objects.equals(id, activity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
