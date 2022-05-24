62ff51de0fda98553ad8ff2fa4288318efd3aaed
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-24 01:16:32 -0300

    update findAll to return ordered by day, start & end times

--- src/main/java/com/sip/api/repositories/TimeslotRepository.java
+++ src/main/java/com/sip/api/repositories/TimeslotRepository.java
@@ -5,11 +5,13 @@ import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
 
 public interface TimeslotRepository extends JpaRepository<Timeslot, String> {
 
+    @Query("select t from Timeslot t ORDER BY t.dayOfWeek, t.startTime, t.endTime ASC")
     @NonNull List<Timeslot> findAll();
 
     @Query("SELECT t " +
