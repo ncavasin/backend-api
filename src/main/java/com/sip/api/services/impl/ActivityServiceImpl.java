04bb367d528b08b3efcf741355ac0ef0ca77195e
Nicolas Cavasin <ncavasin97@gmail.com>
2022-05-27 00:33:57 -0300

    activity: add check for verifying professor has admin or professor role

--- src/main/java/com/sip/api/services/impl/ActivityServiceImpl.java
+++ src/main/java/com/sip/api/services/impl/ActivityServiceImpl.java
@@ -2,12 +2,15 @@ package com.sip.api.services.impl;
 
 
 import com.sip.api.domains.activity.Activity;
+import com.sip.api.domains.role.Role;
+import com.sip.api.domains.user.User;
 import com.sip.api.dtos.activity.ActivityCreationDto;
 import com.sip.api.dtos.activity.ActivityDto;
 import com.sip.api.exceptions.BadRequestException;
 import com.sip.api.exceptions.NotFoundException;
 import com.sip.api.repositories.ActivityRepository;
 import com.sip.api.services.ActivityService;
+import com.sip.api.services.RoleService;
 import com.sip.api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +24,7 @@ import java.util.List;
 public class ActivityServiceImpl implements ActivityService {
     private final ActivityRepository activityRepository;
     private final UserService userService;
+    private final RoleService roleService;
 
     @Override
     public List<Activity> findAll() {
@@ -38,6 +42,8 @@ public class ActivityServiceImpl implements ActivityService {
             throw new BadRequestException("Activity already exists!");
 
         checkNegativities(activityCreationDto.getBasePrice(), activityCreationDto.getAttendeesLimit());
+        checkProfessorAssignedRoles(activityCreationDto.getProfessor().getId());
+
         return activityRepository.save(Activity.builder()
                 .name(activityCreationDto.getName())
                 .basePrice(activityCreationDto.getBasePrice())
@@ -49,12 +55,28 @@ public class ActivityServiceImpl implements ActivityService {
     @Override
     public Activity updateActivity(String activityId, ActivityDto activityDto) {
         Activity activity = findById(activityId);
+
         checkNegativities(activityDto.getBasePrice(), activityDto.getAttendeesLimit());
+        checkProfessorAssignedRoles(activityDto.getProfessor().getId());
+
         activity.setName(activityDto.getName());
         activity.setBasePrice(activityDto.getBasePrice());
         activity.setAttendeesLimit(activityDto.getAttendeesLimit());
         activity.setProfessor(userService.findById(activityDto.getProfessor().getId()));
         return activityRepository.save(activity);
+    }
+
+    private void checkProfessorAssignedRoles(String userId) {
+        if (!userHasAnyOfTheseRoles(userId, List.of(roleService.findByName("ROLE_ADMIN"),
+                roleService.findByName("ROLE_PROFESSOR"))))
+            throw new BadRequestException("User does not have ROLE_PROFESSOR assigned!");
+    }
+
+    private boolean userHasAnyOfTheseRoles(String userId, List<Role> roles) {
+        final User user = userService.findById(userId);
+        return user.getRoles().stream()
+                .anyMatch(role -> roles.stream()
+                        .anyMatch(r -> r.getName().equals(role.getName())));
     }
 
     private void checkNegativities(Double price, int attendeeLimit) {
