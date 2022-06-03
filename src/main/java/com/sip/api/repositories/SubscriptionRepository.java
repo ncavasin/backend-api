047997aa353219bc4855799b37fc16733a981f0d
Nicolas Cavasin <ncavasin97@gmail.com>
2022-06-03 00:16:46 -0300

    SIP19: remove query for method that checks date overlapping of subscriptions

--- src/main/java/com/sip/api/repositories/SubscriptionRepository.java
+++ src/main/java/com/sip/api/repositories/SubscriptionRepository.java
@@ -3,7 +3,9 @@ package com.sip.api.repositories;
 import com.sip.api.domains.subscription.Subscription;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,8 +13,19 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Stri
 
     @NonNull List<Subscription> findAll();
 
+    @Query("delete from Subscription s where s.user.id = ?1")
+    @Transactional
+    @Modifying
+    void deleteAllByUserId(String userId);
+
     @Query("select s from Subscription s where s.user.id = ?1")
     List<Subscription> findAllByUserId(String userId);
+
+    @Query("select s from Subscription s where s.plan.id = ?1")
+    List<Subscription> findAllByPlanId(String planId);
+
+    @Query("select s from Subscription s where s.user.id = ?1 and s.plan.id = ?2")
+    List<Subscription> findAllByUserIdAndPlanId(String userId, String planId);
 
     @Query("select (count(s) > 0) from Subscription s where s.user.id = ?1")
     boolean existsByUserId(String userId);
--- src/main/java/com/sip/api/services/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/sip/api/services/impl/SubscriptionServiceImpl.java
@@ -4,6 +4,7 @@ import com.sip.api.domains.plan.Plan;
 import com.sip.api.domains.subscription.Subscription;
 import com.sip.api.domains.user.User;
 import com.sip.api.dtos.subscription.SubscriptionCreationDto;
+import com.sip.api.exceptions.BadRequestException;
 import com.sip.api.exceptions.NotFoundException;
 import com.sip.api.repositories.SubscriptionRepository;
 import com.sip.api.services.PlanService;
@@ -12,6 +13,7 @@ import com.sip.api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -27,20 +29,32 @@ public class SubscriptionServiceImpl implements SubscriptionService {
     }
 
     @Override
-    public List<Subscription> findSubscriptionsByUserId(String userId) {
-        return subscriptionRepository.findAllByUserId(userId);
-    }
-
-    @Override
     public Subscription findSubscriptionById(String subscriptionId) {
         return subscriptionRepository.findById(subscriptionId)
                 .orElseThrow(() -> new NotFoundException("Subscription not found"));
     }
 
     @Override
+    public List<Subscription> findSubscriptionsByUserId(String userId) {
+        return subscriptionRepository.findAllByUserId(userId);
+    }
+
+    @Override
+    public List<Subscription> findSubscriptionsByPlanId(String planId) {
+        return subscriptionRepository.findAllByPlanId(planId);
+    }
+
+    @Override
     public Subscription createSubscription(SubscriptionCreationDto subscriptionCreationDto) {
         Plan plan = planService.findById(subscriptionCreationDto.getPlanId());
         User user = userService.findById(subscriptionCreationDto.getUserId());
+        if (subscriptionCreationDto.getMonthsToAdd() < 1)
+            throw new BadRequestException("Months to add must be greater than 0");
+
+        final LocalDate endDate = subscriptionCreationDto.getStartDate().plusMonths(subscriptionCreationDto.getMonthsToAdd());
+
+        if (isOverlappingWithUserIdAndPlanIdWithinDates(user.getId(), plan.getId(), subscriptionCreationDto.getStartDate(), endDate))
+            throw new BadRequestException("User new Subscription overlaps with an existing subscription for the same plan within the same period");
 
         return subscriptionRepository.save(Subscription.builder()
                 .description(subscriptionCreationDto.getDescription())
@@ -48,7 +62,26 @@ public class SubscriptionServiceImpl implements SubscriptionService {
                 .endDate(subscriptionCreationDto.getStartDate().plusMonths(subscriptionCreationDto.getMonthsToAdd()))
                 .plan(plan)
                 .user(user)
+                .payment(null)
                 .build());
+    }
+
+    private boolean isOverlappingWithUserIdAndPlanIdWithinDates(String userId, String planId, LocalDate startDate, LocalDate endDate) {
+        List<Subscription> subscriptions = subscriptionRepository.findAllByUserIdAndPlanId(userId, planId);
+
+        if (subscriptions.isEmpty())
+            return false;
+
+        // Verify that dates do not overlap
+        return subscriptions.stream()
+                .anyMatch(subscription ->
+                        ((startDate.isBefore(subscription.getStartDate()) || startDate.isEqual(subscription.getStartDate()))
+                                && (endDate.isAfter(subscription.getStartDate()) || endDate.isEqual(subscription.getStartDate())))
+                                && (endDate.isBefore(subscription.getEndDate()) || endDate.isEqual(subscription.getEndDate()))
+                                ||
+                                ((startDate.isAfter(subscription.getStartDate()) || startDate.isEqual(subscription.getStartDate()))
+                                        && (startDate.isBefore(subscription.getEndDate()) || startDate.isEqual(subscription.getEndDate()))
+                                        && (endDate.isAfter(subscription.getEndDate()) || endDate.isEqual(subscription.getEndDate()))));
     }
 
     @Override
@@ -56,5 +89,10 @@ public class SubscriptionServiceImpl implements SubscriptionService {
         if (!subscriptionRepository.existsById(subscriptionId))
             throw new NotFoundException("Subscription not found");
         subscriptionRepository.deleteById(subscriptionId);
+    }
+
+    @Override
+    public void deleteAllSubscriptionsFromUser(String userId) {
+        subscriptionRepository.deleteAllByUserId(userId);
     }
 }
