d2842e36305dfc4ca071592690a9face737d3994
Nicolas Cavasin <ncavasin97@gmail.com>
2022-06-10 01:14:09 -0300

    SIP81: finish PaymentConverter

--- src/main/java/com/sip/api/domains/payment/PaymentConverter.java
+++ src/main/java/com/sip/api/domains/payment/PaymentConverter.java
@@ -1,5 +1,6 @@
 package com.sip.api.domains.payment;
 
+import com.sip.api.domains.subscription.SubscriptionConverter;
 import com.sip.api.dtos.payment.PaymentDto;
 
 import java.util.List;
@@ -14,8 +15,14 @@ public class PaymentConverter {
     }
 
     public static PaymentDto fromEntityToDto(Payment payment) {
+        if (payment == null) return null;
         return PaymentDto.builder()
                 .id(payment.getId())
+                .transactionId(payment.getTransactionId())
+                .subscriptionDto(SubscriptionConverter.fromEntityToDto(payment.getSubscription()))
+                .paymentDate(payment.getPaymentDate())
+                .amountPaid(payment.getAmountPaid())
+                .paymentStatus(payment.getPaymentStatus())
                 .build();
     }
 }
