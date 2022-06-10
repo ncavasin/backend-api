0da4edbbff4ec85dc3f87d7caaf0084073b2b9d8
Nicolas Cavasin <ncavasin97@gmail.com>
2022-06-10 16:34:05 -0300

    SIP-81: update V3

--- src/main/resources/db/migration/V3__PAYMENT_SUBSCRIPTION_ONE_TO_ONE.sql
+++ src/main/resources/db/migration/V3__PAYMENT_SUBSCRIPTION_ONE_TO_ONE.sql
@@ -1,4 +1,10 @@
 ALTER TABLE IF EXISTS payment
     ADD COLUMN payment_status VARCHAR(255);
 
+ALTER TABLE IF EXISTS payment
+    ADD COLUMN IF NOT EXISTS subscription_id VARCHAR(255) REFERENCES subscription (id);
+
+ALTER TABLE IF EXISTS subscription
+    ADD COLUMN IF NOT EXISTS payment_subscription_id VARCHAR(255) REFERENCES subscription (id);
+
 DROP TABLE IF EXISTS payment_subscriptions;
