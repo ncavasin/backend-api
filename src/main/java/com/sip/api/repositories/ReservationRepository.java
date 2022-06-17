d235d2e8e00214c54309806048af86f5ee6abe78
Nicolas Cavasin <ncavasin97@gmail.com>
2022-06-17 15:13:19 -0300

    SIP112: add query to find user reservations amount

--- src/main/java/com/sip/api/repositories/ReservationRepository.java
+++ src/main/java/com/sip/api/repositories/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.sip.api.repositories;
 
+import com.sip.api.domains.plan.Plan;
 import com.sip.api.domains.reservation.Reservation;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,23 +15,30 @@ public interface ReservationRepository extends JpaRepository<Reservation, String
     List<Reservation> findAll();
 
     @Query(nativeQuery = true,
-    value = "SELECT r.* " +
-            "FROM user_data u " +
-                "JOIN reservation_user_data rud on u.id = rud.user_id " +
-                "JOIN reservation r on rud.reservation_id = r.id " +
-            "WHERE u.id = :userId")
+            value = "SELECT r.* " +
+                    "FROM user_data u " +
+                    "JOIN reservation_user_data rud on u.id = rud.user_id " +
+                    "JOIN reservation r on rud.reservation_id = r.id " +
+                    "WHERE u.id = :userId")
     List<Reservation> findAllByAttendeeId(String userId);
 
     @Query(nativeQuery = true,
             value = "SELECT count(*) " +
                     "FROM reservation r " +
-                        "JOIN reservation_user_data rud ON r.id = rud.reservation_id " +
-                        "JOIN available_class ac ON r.available_class_id = ac.id " +
+                    "JOIN reservation_user_data rud ON r.id = rud.reservation_id " +
+                    "JOIN available_class ac ON r.available_class_id = ac.id " +
                     "WHERE ac.id = :availableClassId")
     Integer countAttendeeAmountByAvailableClassId(String availableClassId);
 
     @Query(value = "SELECT r FROM Reservation r WHERE r.availableClass.id = ?1")
     Optional<Reservation> findByAvailableClass_Id(@NonNull String availableClassId);
+
+    @Query(nativeQuery = true,
+    value = "SELECT COUNT(*) FROM plan p " +
+            "JOIN plan_subscriptions ps ON p.id = ps.plan_id " +
+            "JOIN subscription s ON ps.subscription_id = s.id " +
+            "WHERE s.user_id = :user_id")
+    Integer countReservationsByUserId(String userId);
 
     @Query("SELECT (COUNT(r) > 0) FROM Reservation r WHERE r.id = ?1")
     boolean existsById(@NonNull String reservationId);
